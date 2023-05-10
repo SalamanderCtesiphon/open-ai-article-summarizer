81fb26d1b4b32ade8e76a4e388e164d7784183f8
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-10 13:35:49 -0500

    fixed a small bug with the fetch request

--- src/services/store.js
+++ src/services/store.js
@@ -3,9 +3,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import { articleApi } from "./article";
 
 export const store = configureStore({
-  reducer: {
-    [articleApi.reducerPath]: articleApi.reducer
-  },
-  middleware:(getDefaultMiddleware) =>
-  getDefaultMiddleware().concat()
+    reducer: {
+        [articleApi.reducerPath]: articleApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware)
 })
