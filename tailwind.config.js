739fa1d9443a5cbbeda464aa2a506c381de2fe99
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-10 12:19:19 -0500

    i had to fix a typo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  content: [],
   theme: {
     extend: {
       fontFamily: {
